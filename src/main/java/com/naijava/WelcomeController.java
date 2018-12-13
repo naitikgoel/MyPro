459a16be0fdbdafc3b0ae9a33c93968356b10773
Goel <717687@cognizant.com>
2018-12-13 15:46:13 +0530

    First commit

---
+++ src/main/java/com/naijava/WelcomeController.java
@@ -0,0 +1,10 @@
+package com.naijava;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class WelcomeController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return"Hello!";
+    }
+}
